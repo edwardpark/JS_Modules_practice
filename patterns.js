e87568a5019bd30796b64140f9b81418a3c79af7
oldmaneddie <parkes.edward@gmail.com>
2015-08-17 00:15:33 +0000

    added basic html type and underscore cdn + created new module

--- patterns.js
+++ patterns.js
@@ -1,8 +1,26 @@
-//wrapped function inside of a functional enclosure
+var awesomeNewModule = (function(){ 
+  var exports = {
+    foo: 5,
+    bar: 10
+  };
+  
+  exports.helloMars = function() {
+    console.log("Hello Mars!");
+  };
+  
+  exports.goodbye = function() {
+    console.log("Goodbye!");
+  };
+  
+  return exports
+  
+}());
 
-(function(){
-    function foo(){
-        console.log("function within a enclosure");
-    }
-    foo();
-}());//by wrapping function in parenthesis treats like a free  expression
+// //wrapped function inside of a functional enclosure
+
+// !function(){
+//     function foo(){
+//         console.log("function within a enclosure");
+//     }
+//     foo();
+// }();//by wrapping function in parenthesis treats like a free  expression
